a1ece7c4e7420635a5ee0f3f474f6219d41d7ec9
Majun1990 <merijun_a@hotmail.com>
2019-08-21 01:47:26 +0000

    cleaned conflict

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,26 +1,5 @@
 class BooksController < ApplicationController
     def index
-<<<<<<< HEAD
-        @books = Books.all
-    end
-
-    def create
-        @book = Book.new
-        
-    end
-
-    def update
-        @book = Book.find(params[:id])
-        
-    end
-
-    def show
-        @book = Book.find(params[:id])
-    end
-
-    def destroy
-        @book = Book.find(params[:id])
-=======
         @books = Book.all
     end
     
@@ -43,12 +22,9 @@ class BooksController < ApplicationController
         redirect_to books_path
     end
 
-
-
     def destroy
         @book = Book.find(params[:id])
         @book.destroy
         redirect_to books_path
->>>>>>> part-01-initial-app
     end
 end
